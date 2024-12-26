a975ce8d7ab3e09905fce6c1fa2790d1df3cc5a0
Omar Eloui <contact@omareloui.com>
2024-12-26 14:09:24 +0200

    chore(init): init a nuxt project

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script setup>
+// Pre-render the homepage at build time
+// defineRouteRules({
+//   prerender: true,
+// });
+</script>
+
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
