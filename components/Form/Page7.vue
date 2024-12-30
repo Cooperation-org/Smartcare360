cc3dc70ecbf1262e72a127f5a895011afe2be2a9
Omar Eloui <contact@omareloui.com>
2024-12-30 20:21:03 +0200

    feat(form): add missing pages - without content

---
+++ components/Form/Page7.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { z } from "zod";
+import type { FormSubmitEvent } from "#ui/types";
+
+type ReturnUseFormState = ReturnType<typeof useFormState>;
+type Schema = z.output<ReturnUseFormState["schema"]>;
+
+const { onSubmit, state, schema } = defineProps<{
+  onSubmit: (event: FormSubmitEvent<Schema>) => void;
+  state: ReturnUseFormState["state"];
+  schema: ReturnUseFormState["schema"];
+}>();
+</script>
+
+<template>
+  <UForm :schema="schema" :state="state" @submit.prevent="onSubmit">
+    page 7
+  </UForm>
+</template>
