e3daaec47c95d3d5db64618b8992d7312a6b18c5
Omar Eloui <contact@omareloui.com>
2024-12-31 15:16:34 +0200

    feat(page-7): add page 7

--- constants/languages.ts
+++ constants/languages.ts
@@ -1,1 +1,7 @@
 export const LANGUAGES = ["English", "Arabic"] as const;
+export const RESPONSE_TIME_FOR_BACKUP = [
+  { value: "2-hours", label: "Within 2 hours" },
+  { value: "today", label: "Today" },
+  { value: "48-hours", label: "Within 48 hours" },
+  { value: "next-visit", label: "Next scheduled visit" },
+];
